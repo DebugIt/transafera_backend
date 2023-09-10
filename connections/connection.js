099920fb832568b9027a8b189ff1df5237ed412f
Ronak A. Bhanushali <92251593+DebugIt@users.noreply.github.com>
2023-09-10 20:41:43 +0530

    Update connection.js

--- connections/connection.js
+++ connections/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config()
 
-mongoose.connect(process.env.MONGOURI).then(
+mongoose.connect(`${process.env.MONGOURI}`).then(
     () => {
         console.log('Connection to DB success!')
     }
